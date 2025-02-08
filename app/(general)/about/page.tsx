14d087c2a7833f36f87bcd2a98dfd6c11f67d434
Andres Torres <andtorrcan94@gmail.com>
2025-02-07 21:58:43 -0500

    add: more changes

--- app/(general)/about/page.tsx
+++ app/(general)/about/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-   title: 'SEO Title',
+   title: 'About us',
    description: 'SEO Desc',
    keywords: ["chicken","test"]
 };
--- app/(general)/princing/page.tsx
+++ app/(general)/princing/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'SEO Princing',
+    title: 'Princing',
     description: 'Princing Desc',
     keywords: ["price","dollar"]
  };
